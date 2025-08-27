87288e86e22b4c57b133e853a6063f32b6d722a8
Alongkorn C <alongkorn.c@kbtg.tech>
2025-08-27 11:25:43 +0700

    commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+      <p>This is a Vite + React project created inside the requested directory.</p>
+    </div>
+  )
+}
